3a33415534a50a7a931561e5f84382acb495ee75
ov105 <tim.heath.lee@gmail.com>
2023-11-25 23:15:10 -0500

    add src to pyproject.toml, add freeze

--- src/icechest/icechest.py
+++ src/icechest/icechest.py
@@ -1,17 +1,45 @@
 import argparse
+import dataclasses
 from dataclasses import dataclass
 import hashlib
-from pathlib import Path
+import json
 import logging
 import os
+from pathlib import Path
 import sys
 import shutil
+import time
+from typing import Optional
 from zipfile import ZipFile
+
+import boto3
+import botocore
+
+JOB_HISTORY_FILE = "icebox.json"
 
 @dataclass
 class ArchiveFile:
     path: Path
     checksum: str
+
+@dataclass
+class GlacierArchive:
+    local_path: Path
+    checksum: str
+    vault_name: str
+    id: str
+
+@dataclass
+class GlacierJob:
+    type: str
+    id: str
+    timestamp: int
+    vault_name: str
+    status: str 
+    data: str = ""
+
+    def set_data(self, obj: object) -> None:
+        self.data = str(object)
 
 def thaw(vault:str, archive:str) -> str:
     pass
@@ -46,7 +74,46 @@ def create_archive(source:str) -> ArchiveFile:
     logging.debug(f"{digest = }") 
     return ArchiveFile(archive_path, digest) 
 
-def freeze(source:str, vault:str) -> str:
+def get_resource(region_name: str=None) -> object:
+    """get glacier resource
+
+    Args:
+        region_name (str, optional): AWS region. Defaults to None.
+
+    Returns:
+        object: glacier.ServiceResource
+    """
+    if region_name is None:
+        region_name = os.getenv('AWS_DEFAULT_REGION', default='us-east-1')
+
+    logging.debug(f"{region_name =}")
+    return boto3.resource("glacier", region_name=region_name)
+
+def get_vault_resource(vault_name: str, region_name: str=None) -> object:
+    resource = get_resource(region_name)
+    return resource.Vault('-', vault_name)
+
+def save_job(job: GlacierJob, hist_file_path: Path=None) -> str:
+    if hist_file_path is None:
+        hist_file_path = Path().expanduser() / JOB_HISTORY_FILE
+
+    logging.debug(f"Updating {hist_file_path} with {job}")
+    try:
+        with open(hist_file_path, 'r') as jf:
+            history = json.load(jf)
+    except FileNotFoundError:
+        history = [] 
+
+    history.append(dataclasses.asdict(job))
+    with open(hist_file_path, 'w') as jf:
+        jf.write(json.dumps(history))
+
+    return history
+
+def get_inprogress_jobs(job_type: str) -> list[str]:
+    return []
+
+def freeze(source:str, vault_name:str, aws_region: Optional[str]=None) -> str:
     """Create Zip file containing file(s) located at source and save 
        Zip in AWS Glacier vault.
 
@@ -58,8 +125,25 @@ def freeze(source:str, vault:str) -> str:
         str: zip file path
     """
     archive = create_archive(source)
-    return archive
+    vault = get_vault_resource(vault_name, aws_region)
+    frozen = vault.upload_archive(archiveDescription=f"", body=str(archive.path))
+    logging.info(f"Uploaded {archive.path} with ID: {frozen.id} to {vault.name}")
+    return GlacierArchive(archive.path, archive.checksum, vault_name, frozen.id)
     
+def get_vaults(region_name=None):
+    resource = get_resource("glacier", region_name=region_name) 
+    return resource.vaults.all()
+
+def print_vault_names(region_name=None):
+    for vault in get_vaults(region_name):
+        print(f"Vault name: {vault.name}")
+
+def save_item(item):
+    pass
+
+def retrieve_items(item):
+    pass
+
 
 def main(args_in):
     """process command line arguments.
@@ -74,7 +158,12 @@ def main(args_in):
     logging.basicConfig(filename="icechest.log",  
                         format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
                         datefmt='%H:%M:%S')
-    freeze(args.source, args.vault)
+    try:
+        archive = freeze(args.source, args.vault)
+    except botocore.exceptions.NoCredentialsError as ex:
+        print(f"Unable to find AWS credentials, set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables or run `aws configure` command.")
+    except Exception as ex:
+        logging.exception(f"{source =} {vault_name =}")
 
 if __name__ == "__main__":
     main(sys.argv[:1])
--- tests/test_icechest.py
+++ tests/test_icechest.py
@@ -1,9 +1,17 @@
+import dataclasses  
+import json
+import os
 import pytest
 from pathlib import Path
 import sys
 
 from icechest import icechest
+import botocore
+
 CONTENT = "MY CONTENT"
+
+VAULT_NAME = "TestOne"
+
 def test_main():
     icechest.main()
 
@@ -12,6 +20,20 @@ def test_file(tmp_path):
     test = tmp_path / "foo"
     test.write_text(CONTENT)
     return test
+
+@pytest.fixture
+def hist_file(tmp_path):
+    hist_file = tmp_path / "hist.json"
+    history = [dataclasses.asdict(icechest.GlacierJob("test", "123abc", 654321, VAULT_NAME, "STARTED"))]
+    with open(hist_file, 'w') as hf:
+        json.dump(history, hf)
+
+    return hist_file
+
+def set_aws_env():
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+    os.environ['AWS_ACCESS_KEY_ID'] = 'FOO'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'BAR'
 
 def test_create_archive_file(test_file):
     archive = icechest.create_archive(str(test_file))
@@ -31,11 +53,57 @@ def test_create_archive_dir(test_file):
     assert archive_path.exists()
     assert archive.checksum is not None
     
+#@pytest.mark.skipif(os.getenv("AWS_SECRET_ACCESS_KEY") is not None, reason="AWS credentials set")
+@pytest.mark.skip("")
+def test_freeze_no_creds(tmp_path):
+    src = tmp_path / "foo"
+    src.write_text(CONTENT)
+    with pytest.raises(botocore.exceptions.NoCredentialsError):
+        icechest.freeze(str(src), VAULT_NAME)
+
+#@pytest.mark.skipif(os.getenv("AWS_SECRET_ACCESS_KEY") is None, reason="AWS credentials NOT set")
 def test_freeze(tmp_path):
     src = tmp_path / "foo"
     src.write_text(CONTENT)
-    vault = "bar"
-    icechest.freeze(str(src), vault)
+    icechest.freeze(str(src), VAULT_NAME)
+
+@pytest.mark.skipif(os.getenv("AWS_SECRET_ACCESS_KEY") is None, reason="AWS credentials NOT set")
+def test_get_vaults():
+    vaults = icechest.get_vaults()
+    assert len(vaults) > 0
+
+def test_get_resource():
+    set_aws_env()
+    res = icechest.get_resource()
+    assert str(res) == 'glacier.ServiceResource()'
+
+def test_save_job_existing_file(hist_file):
+    job = icechest.GlacierJob("test_type", "def456", 123456, "my_vault", "STARTED")
+    rtn = icechest.save_job(job, hist_file)
+    assert rtn is not None
+    assert len(rtn) == 2
+    assert rtn[0]['timestamp'] == 654321
+    assert rtn[1]['timestamp'] == 123456
+    assert hist_file.exists()
+    with open(hist_file, 'r') as jf:
+        history = json.load(jf)
+    assert len(history) == 2
+    assert history[1]['timestamp'] == 123456
+    assert history[0]['timestamp'] == 654321
+
+def test_save_job_no_file(tmp_path):
+    job = icechest.GlacierJob("test_type", "abc123", 123456, "my_vault", "STARTED")
+    hist_file = tmp_path / "hist.json"
+    assert not hist_file.exists()
+    rtn = icechest.save_job(job, hist_file)
+    assert rtn is not None
+    assert len(rtn) == 1
+    assert rtn[0]['timestamp'] == 123456
+    assert hist_file.exists()
+    with open(hist_file, 'r') as jf:
+        history = json.load(jf)
+    assert len(history) == 1
+    assert history[0]['timestamp'] == 123456
 
 @pytest.mark.skip(reason="not implemented")
 def test_main():
